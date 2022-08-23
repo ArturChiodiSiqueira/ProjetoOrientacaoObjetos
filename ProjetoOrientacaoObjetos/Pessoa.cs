aff081dda4e3466516ecae2580541e93667ec0c2
artur <artur@DESKTOP-V0JU5PP>
2022-08-23 16:54:36 -0300

    criacao dos metodos e chamada pelos objetos

--- ProjetoOrientacaoObjetos/Pessoa.cs
+++ ProjetoOrientacaoObjetos/Pessoa.cs
@@ -13,5 +13,28 @@ namespace ProjetoOrientacaoObjetos
         public int Cpf { get; set; }
         public int Telefone { get; set; }
 
+        public void ImplementaNome()
+        {
+            Console.Write("Informe seu nome: ");
+            Nome = Console.ReadLine();
+        }
+
+        public void ImolementaDataNascimento()
+        {
+            Console.Write("Informe sua data de nascimento: ");
+            DataNascimento = Console.ReadLine();
+        }
+
+        public void ImplementaCPF()
+        {
+            Console.Write("Informe seu cpf: ");
+            Cpf = int.Parse(Console.ReadLine());
+        }
+
+        public void ImplementaTelefone()
+        {
+            Console.Write("Informe seu telefone: ");
+            Telefone = int.Parse(Console.ReadLine());
+        }
     }
 }
--- ProjetoOrientacaoObjetos/Program.cs
+++ ProjetoOrientacaoObjetos/Program.cs
@@ -7,8 +7,26 @@ namespace ProjetoOrientacaoObjetos
         static void Main(string[] args)
         {
             Pessoa pessoa1 = new Pessoa();
-            pessoa1.Nome = "Fabio";
-            Console.WriteLine();
+
+            pessoa1.ImplementaNome();
+            pessoa1.ImolementaDataNascimento();
+            pessoa1.ImplementaCPF();
+            pessoa1.ImplementaTelefone();
+
+            Pessoa pessoa2 = new Pessoa();
+
+            pessoa2.ImplementaNome();
+            pessoa2.ImolementaDataNascimento();
+            pessoa2.ImplementaCPF();
+            pessoa2.ImplementaTelefone();
+
+            Pessoa pessoa3 = new Pessoa();
+
+            pessoa3.ImplementaNome();
+            pessoa3.ImolementaDataNascimento();
+            pessoa3.ImplementaCPF();
+            pessoa3.ImplementaTelefone();
+            Console.ReadKey();
         }
     }
 }
