f22b1e8db48e952eba9141a7d1572212711b8893
artur <artur@DESKTOP-V0JU5PP>
2022-08-23 11:28:51 -0300

    Add project files.

---
+++ ProjetoOrientacaoObjetos/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProjetoOrientacaoObjetos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
